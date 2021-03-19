e2855543450124e5c394f6e61380c8fd4930d2a8
Benriez <33022072+Benriez@users.noreply.github.com>
2021-03-19 14:37:11 +0100

    update all

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex column" :class="bgClass">
+  <q-page class="flex column topSearch" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
       <q-input
         @keyup.enter="getWeatherbySearch"
@@ -30,7 +30,7 @@
     
 
     <template v-if="weatherData">
-      <div class="col text-white text-center">
+      <div class="col text-white text-center textData">
         <div class="text-h4 text-weight-light">
           {{weatherData.name}}
         </div>
@@ -53,12 +53,12 @@
 
     <template v-else>
       <div class="col column text-center text-white">
-        <div class="col text-h2 text-weight-thin">
+        <div class="col text-h2 text-weight-thin intro">
           What's my <br> Incidence
         </div>
           <q-btn
             @click="getLocation"
-            class="col"
+            class="col geLocationBtn"
             flat>
             <q-icon left size="3em" name="my_location" />
             <div>Find my location</div>
@@ -66,7 +66,20 @@
       </div>
     </template>
 
-    <div class="col skyline"></div>
+    <div class="wrapper">
+      <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
 
   </q-page>
 </template>
@@ -175,17 +188,106 @@ export default {
 
 <style lang="sass">
   .q-page
-    background: linear-gradient(to top, #00b4db, #0083b0)
+    background: linear-gradient(to top, #712c57, #000000)
     &.bg-night
-      background: linear-gradient(to bottom, #16222a, #3a6073)
+      background: linear-gradient(to bottom, #712c57, #000000)
     &.bg-day
-      background: linear-gradient(to top, #00b4db, #0083b0)
+      background: linear-gradient(to top, #712c57, #000000)
   .degree
-    top: -44px;
+    top: -44px
 
   // .skyline
   //   flex: 0 0 150px
   //   background: url(../assets/skyline.png)
   //   background-size: contain
   //   background-position: center bottom
+
+  body
+    margin: 0
+    padding: 0
+
+  .geLocationBtn
+    z-index: 10
+  
+  .topSearch
+    z-index: 11
+  
+  .intro
+    margin-top: -5rem
+
+  .wrapper
+    position: absolute
+    width: 100%
+    height: 100%
+    overflow: hidden
+    z-index: -1
+  
+  .textData
+    margin-top: -5rem
+  
+  .box div
+    position: absolute
+    width: 60px
+    height: 60px
+    background-color: transparent
+    border: 6px solid #efefef
+
+  .box div:nth-child(1)
+    top: 12%
+    left: 42%
+    animation: animate 10s linear infinite
+  
+  .box div:nth-child(2)
+    top: 85%
+    left: 50%
+    animation: animate 7s linear infinite
+  
+  .box div:nth-child(3)
+    top: 17%
+    left: 6%
+    animation: animate 9s linear infinite
+  
+  .box div:nth-child(4)
+    top: 20%
+    left: 60%
+    animation: animate 10s linear infinite
+  
+  .box div:nth-child(5)
+    top: 67%
+    left: 10%
+    animation: animate 6s linear infinite
+  
+  .box div:nth-child(6)
+    top: 80%
+    left: 70%
+    animation: animate 12s linear infinite
+  
+  .box div:nth-child(7)
+    top: 60%
+    left: 80%
+    animation: animate 15s linear infinite
+  
+  .box div:nth-child(8)
+    top: 32%
+    left: 25%
+    animation: animate 16s linear infinite
+
+  .box div:nth-child(9)
+    top: 90%
+    left: 25%
+    animation: animate 9s linear infinite
+  
+  .box div:nth-child(10)
+    top: 20%
+    left: 80%
+    animation: animate 5s linear infinite
+
+  @keyframes animate
+    0%
+      transform: scale(0) translateY(0) rotate(0)
+      opacity: 1
+    100%
+      transform: scale(1.3) translateY(-90px) rotate(360deg)
+      opacity: 0
+
 </style>
